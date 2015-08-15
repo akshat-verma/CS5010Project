d26c76488f9ecdb8933f2aea98bda4a2b8939549
AkshatVerma <aksv0612@gmail.com>
2015-08-15 09:48:29 -0400

    Final Version

--- project/analysis.py
+++ project/analysis.py
@@ -1,162 +1,174 @@
 '''
 Created on Aug 11, 2015
-
-@author: akshat
+CS5010 Project
+@author: Akshat Verma (av2zf)
+         Paul Cherian (pmc6dn)
+         Prateek Agarwal (pa7sb)
 '''
-import pandas as pd
-import statsmodels.formula.api as sm
-from bokeh.browserlib import view
-from bokeh.models.widgets import VBox,HBox
-from bokeh.models import RadioButtonGroup,ColumnDataSource,HoverTool
-from bokeh.document import Document
-from bokeh.session import Session
-from bokeh.plotting import figure
-from bokeh.charts import Bar
-from bokeh.sampledata import us_counties
+
 import math
 import operator
 
+from bokeh.browserlib import view
+from bokeh.charts import Bar
+from bokeh.document import Document
+from bokeh.models import RadioButtonGroup, ColumnDataSource, HoverTool
+from bokeh.models.widgets import VBox, HBox
+from bokeh.plotting import figure
+from bokeh.sampledata import us_counties  # might have to download the data the first time
+from bokeh.session import Session
+
+import pandas as pd
+import statsmodels.formula.api as sm
 
 
-df = pd.read_csv("indicators.csv")
-df = pd.DataFrame({'Value' : df.groupby( [ "County", "Indicator Type"] )['Value'].mean()}).reset_index()
-df = df.pivot_table("Value",["County"],"Indicator Type").reset_index()
-df.set_index('County',inplace=True)
+col_dic = {0:"Cancer_Deaths", 1:"Heart_Disease_Deaths", 2:"Respiratory_Disease_Deaths", 3:"Diabetes_Deaths"}
 
+# Function to normalize the columns in a dataframe
 def normalize(df):
     normalized = df.copy()
     for col_name in df.columns:
         max_val = df[col_name].max()
         min_val = df[col_name].min()
-        normalized[col_name] = (df[col_name] - min_val) / (max_val- min_val)
+        normalized[col_name] = (df[col_name] - min_val) / (max_val - min_val)
     return normalized
 
+# Function to generate regression models and return a list of models
+def generate_regression_models(df):
+    # Using glm function in statsmodels.formula.api class to create regression models
+    heart_deaths = sm.glm(formula="Heart_Disease_Deaths ~ Obesity + Binge_Drinking + Smoking + Primary_Care + No_Insurance + Median_Household_Income + College_Degrees + Long_Term_Care_Hospital_Admissions + Unemployed_Persons + Liquor_Stores", data=df).fit()
+    cancer_deaths = sm.glm(formula="Cancer_Deaths ~ Obesity + Binge_Drinking + Smoking + Primary_Care + No_Insurance + Median_Household_Income + College_Degrees + Long_Term_Care_Hospital_Admissions + Unemployed_Persons + Liquor_Stores", data=df).fit()
+    diabetes_deaths = sm.glm(formula="Diabetes_Deaths ~ Obesity + Smoking + Binge_Drinking + Primary_Care + No_Insurance + Median_Household_Income + College_Degrees + Long_Term_Care_Hospital_Admissions + Unemployed_Persons + Liquor_Stores", data=df).fit()
+    resp_deaths = sm.glm(formula="Respiratory_Disease_Deaths ~ Obesity + Smoking + Binge_Drinking + Primary_Care + No_Insurance + Median_Household_Income + College_Degrees + Long_Term_Care_Hospital_Admissions + Unemployed_Persons + Liquor_Stores", data=df).fit()
+   
+    # Appending the different models to a list
+    models = []
+    models.append(heart_deaths)
+    models.append(cancer_deaths)
+    models.append(resp_deaths)
+    models.append(diabetes_deaths)
+    return models
 
-df = normalize(df)
+# Populate a list of tuple-list where each tuple-list has variable name and variable importance
+def populate_factor_list(df):
+    factor_list = []
+    models = generate_regression_models(df)
+    for model in models:
+        p_dict = dict(zip(model.pvalues.index, model.pvalues))
+        params_dict = dict(zip(model.params.index, model.params))
+        filtered_dict = {k:[abs(v)] for (k, v) in params_dict.items() if p_dict.get(k) < 0.1 and k is not 'Intercept'}
+        sorted_list = sorted(filtered_dict.items(), key=operator.itemgetter(1), reverse=True)
+        if len(sorted_list) > 2:
+            sorted_list = sorted_list[0:2] 
+        factor_list.append(sorted_list)
+    return factor_list
 
-
-'''
-result = sm.ols(formula="Heart_Deaths ~ Obesity", data=df).fit()
-print(dict(zip(result.pvalues.index,result.pvalues)))
-'''
-#Regression
-
-heart_deaths = sm.glm(formula="Heart_Disease_Deaths ~ Obesity + Binge_Drinking + Smoking + Primary_Care + No_Insurance + Median_Household_Income + College_Degrees + Long_Term_Care_Hospital_Admissions + Unemployed_Persons + Liquor_Stores", data=df).fit()
-cancer_deaths = sm.glm(formula="Cancer_Deaths ~ Obesity + Binge_Drinking + Smoking + Primary_Care + No_Insurance + Median_Household_Income + College_Degrees + Long_Term_Care_Hospital_Admissions + Unemployed_Persons + Liquor_Stores", data=df).fit()
-diabetes_deaths = sm.glm(formula="Diabetes_Deaths ~ Obesity + Smoking + Binge_Drinking + Primary_Care + No_Insurance + Median_Household_Income + College_Degrees + Long_Term_Care_Hospital_Admissions + Unemployed_Persons + Liquor_Stores", data=df).fit()
-resp_deaths = sm.glm(formula="Respiratory_Disease_Deaths ~ Obesity + Smoking + Binge_Drinking + Primary_Care + No_Insurance + Median_Household_Income + College_Degrees + Long_Term_Care_Hospital_Admissions + Unemployed_Persons + Liquor_Stores", data=df).fit()
-
-models = []
-models.append(heart_deaths)
-models.append(cancer_deaths)
-models.append(diabetes_deaths)
-models.append(resp_deaths)
-
-#models.append(heart_deaths).append(cancer_deaths).append(diabetes_deaths).append(hiv_deaths)
-
-factor_list = []
-for model in models:
-    p_dict = dict(zip(model.pvalues.index,model.pvalues))
-    params_dict = dict(zip(model.params.index,model.params))
-    filtered_dict = {k:[abs(v)] for (k,v) in params_dict.items() if p_dict.get(k) < 0.1 and k is not 'Intercept'}
-    sorted_list = sorted(filtered_dict.items(), key=operator.itemgetter(1),reverse = True)
-    if len(sorted_list) >2:
-        sorted_list = sorted_list[0:2] 
-    factor_list.append(sorted_list)
-    
-
-factor_dict=[dict(x) for x in factor_list]
-
-
-
-us_counties=us_counties.data.copy()
-fips_list = [ (a*1000+b)for (a,b) in us_counties.keys() if a is 51]
-
-
-
-va_xs=[us_counties[x]["lons"] for x in us_counties if us_counties[x]["state"]=='va']
-va_ys=[us_counties[y]["lats"] for y in us_counties if us_counties[y]["state"]=='va']
-
-
-va_x=[]
-for l in va_xs:
-    va_x.append([a for a in l if not(math.isnan(a))])
-
-va_y=[]
-for l in va_ys:
-    va_y.append([a for a in l if not(math.isnan(a))])
-
-
-
-county_nm=pd.Series([us_counties[x]["name"] for x in us_counties if us_counties[x]["state"]=='va'])
-
-data_counties=pd.DataFrame(index=pd.Series(fips_list))
-data_counties=pd.concat([data_counties,df],axis=1)
-influencers=[dict(Liquor=[30],Parks=[20]),dict(ambience=[30],hospital_reach=[20])]
-
-top_qtl=df.quantile(0.66)
-middle_qtl=df.quantile(0.33)
-
-session=Session()
-document=Document()
-session.use_doc('python_project')
-session.load_document(document)
-
-col_dic =  {0:"Cancer_Deaths", 1:"Heart_Disease_Deaths", 2:"Respiratory_Disease_Deaths", 3:"Diabetes_Deaths"}
-
+# Function to update plot data,on radio button click 
 def update(a):    
 
-    global county_colors
+    # clearing document contents
     document.clear()
-    hover=HoverTool(tooltips=[("County Name","@i")])
+    # hover tool and figure intialization
+    hover = HoverTool(tooltips=[("County Name", "@i"), ("Value" , "@v")])
     p = figure(title="US Health Indicator-Virginia", toolbar_location="left",
-           plot_width=900, plot_height=600,tools=[hover])
+           plot_width=900, plot_height=600, tools=[hover])
 
-    county_colors=[]    
+    county_colors = []   
+    # assigning colors to each county according to their values 
     for county in data_counties[col_dic.get(a)]:
-        if county>top_qtl[col_dic.get(a)] :
+        if county > top_qtl[col_dic.get(a)] :
             county_colors.append("red")
-        elif county>middle_qtl[col_dic.get(a)]:
-            county_colors.append( "yellow")
+        elif county > middle_qtl[col_dic.get(a)]:
+            county_colors.append("yellow")
         elif not math.isnan(county):
             county_colors.append("green")
         else:
             county_colors.append("white") 
-    source.data=dict(x=va_x,y=va_y,c=county_colors,i=county_nm)    
+    # updating plotting data        
+    source.data = dict(x=va_x, y=va_y, c=county_colors, i=county_nm, v=[round(val, 2) for val in (data_counties[col_dic.get(a)])])    
          
-    data_counties_map=data_counties.reset_index()
-    data_counties_map['color']=pd.Series(county_colors)
-    data_counties_map.sort(col_dic.get(a),inplace=True)    
-    ind_top3=list(data_counties_map.index[0:3])
-    data_counties_map.sort(col_dic.get(a),inplace=True,ascending=False)
-    ind_bot3=list(data_counties_map.index[0:3])  
+    data_counties_map = data_counties.reset_index()
+    data_counties_map['color'] = pd.Series(county_colors)
+    data_counties_map.sort(col_dic.get(a), inplace=True)    
+    ind_top3 = list(data_counties_map.index[0:3])
+    data_counties_map.sort(col_dic.get(a), inplace=True, ascending=False)
+    ind_bot3 = list(data_counties_map.index[0:3])  
     
     
-    p.patches(xs='x',ys='y', fill_color='c',source=source,alpha=0.5)
-
+    p.patches(xs='x', ys='y', fill_color='c', source=source, alpha=0.5)
+    
+    # coloring top3 and bottom3 counties in mortality rates
     for i in ind_top3:
-        p.patch(x=va_xs[i],y=va_ys[i],fill_color=data_counties_map.ix[i,'color'],line_color="black",line_width=2)
+        p.patch(x=va_xs[i], y=va_ys[i], fill_color=data_counties_map.ix[i, 'color'], line_color="black", line_width=2)
     
     for i in ind_bot3:
-        p.patch(x=va_xs[i],y=va_ys[i],fill_color=data_counties_map.ix[i,'color'],line_color="blue",line_width=2)
+        p.patch(x=va_xs[i], y=va_ys[i], fill_color=data_counties_map.ix[i, 'color'], line_color="blue", line_width=2)
 
-    bar=Bar(factor_dict[a],title="Top Influencers",legend="top_left",height=500,width=500)
-    layout=VBox(children=[radio,p])
-    layout1=HBox(children=[layout,bar])
+    bar = Bar(factor_dict[a], title="Top Influencers", legend="top_right", height=600, width=450)
+   
+    layout = VBox(children=[radio, p])
+    layout1 = HBox(children=[layout, bar])
     document.add(layout1)
+    # storing document in the session/bokeh server
     session.store_document(document)
-        
-    
-source=ColumnDataSource(data=dict(x=[],y=[],c=[],i=[]))
-inf_source=ColumnDataSource(data=dict(facs=[]))
 
-radio=RadioButtonGroup(labels=['Cancer Deaths','Heart Disease Deaths', 'Respiratory Disease Deaths', 'Diabetes Deaths'],active=0)
+# Read data from csv file into pandas dataframe
+df = pd.read_csv("indicators.csv")
+# Grouping by County and Indicator and type and calulating mean of value field to output time averaged value field for each County and indicator type
+df = pd.DataFrame({'Value' : df.groupby([ "County", "Indicator Type"])['Value'].mean()}).reset_index()
+# Transposing the dataframe so that distinct Indicator Types become column names
+df = df.pivot_table("Value", ["County"], "Indicator Type").reset_index()
+# Setting county code as the index
+df.set_index('County', inplace=True)
+# Storing raw dataframe in raw_df
+raw_df = df
+# Normalizing the columns of df
+df = normalize(df)
+# Populating a list of dictionary where each dictionary has variable name and variable importance as key value pair
+factor_dict = [dict(x) for x in populate_factor_list(df)]
+
+
+us_counties = us_counties.data.copy()
+# generating FIPS code for counties in VA
+fips_list = [ (a * 1000 + b)for (a, b) in us_counties.keys() if a is 51]
+# retrieving latitude and longitude values for counties in Virginia State
+va_xs = [us_counties[x]["lons"] for x in us_counties if us_counties[x]["state"] == 'va']
+va_ys = [us_counties[y]["lats"] for y in us_counties if us_counties[y]["state"] == 'va']
+va_x = []
+for l in va_xs:
+    va_x.append([a for a in l if not(math.isnan(a))])
+
+va_y = []
+for l in va_ys:
+    va_y.append([a for a in l if not(math.isnan(a))])
+
+county_nm = pd.Series([us_counties[x]["name"] for x in us_counties if us_counties[x]["state"] == 'va'])
+
+data_counties = pd.DataFrame(index=pd.Series(fips_list))
+data_counties = pd.concat([data_counties, raw_df], axis=1)
+
+# getting quantile values for categorising counties
+top_qtl = raw_df.quantile(0.66)
+middle_qtl = raw_df.quantile(0.33)
+
+session = Session()
+document = Document()
+session.use_doc('python_project')
+session.load_document(document)
+
+source = ColumnDataSource(data=dict(x=[], y=[], c=[], i=[]))
+inf_source = ColumnDataSource(data=dict(facs=[]))
+
+# radio buttons intialization
+radio = RadioButtonGroup(labels=['Cancer Deaths', 'Heart Disease Deaths', 'Respiratory Disease Deaths', 'Diabetes Deaths'], active=0)
 radio.on_click(update)
 
+# calling update function to intialize the plot 
 update(0)
 
+        
 
-if __name__=='__main__':
+if __name__ == '__main__':
     link = session.object_link(document.context)
     print("Please visit %s to see the plots" % link)
     view(link)
--- project/get_data.py
+++ project/get_data.py
@@ -1,35 +1,54 @@
 '''
 Created on Aug 9, 2015
-
-@author: akshat
+CS 5010 Project
+@author: Akshat Verma(av2zf)
+         Paul Cherian(pmc6dn)
+         Prateek Agarwal(pa7sb)
 '''
 
-import requests
 from xml.etree import ElementTree
 
-indicator_map = {15:"Obesity", 13:"Smoking", 25:"Primary_Care", 34:"College_Degrees", 23:"No_Insurance", 10003:"Median_Household_Income", 51:"Liquor_Stores", 200:"Long_Term_Care_Hospital_Admissions",35:"Unemployed_Persons",50011:"Diabetes_Deaths", 486:"Cancer_Deaths", 83:"Heart_Disease_Deaths", 50012:"Respiratory_Disease_Deaths" }
+import requests
 
+
+# Dictionary of Indicator Description Id's (as in the API) and the indicator names
+indicator_map = {15:"Obesity", 13:"Smoking", 25:"Primary_Care", 34:"College_Degrees", 23:"No_Insurance", 10003:"Median_Household_Income", 51:"Liquor_Stores", 200:"Long_Term_Care_Hospital_Admissions", 35:"Unemployed_Persons", 50011:"Diabetes_Deaths", 486:"Cancer_Deaths", 83:"Heart_Disease_Deaths", 50012:"Respiratory_Disease_Deaths" }
+
+# Authorization key to communicate with the API
 auth = "Key=5346643abe134855907383ecf8bd7db0"
 
-f = open("indicators.csv", "a")
-for ind in indicator_map:
-    response = requests.get("http://services.healthindicators.gov/v5/REST.svc/IndicatorDescription/" + str(ind) + "/Indicators/PageCount?" + auth)
-    if response.status_code == 200:
-        tree = ElementTree.fromstring(response.content)
-        page_count = str(tree.find('Data').text)
-        for i in range(1, int(page_count)):
-            url = "http://services.healthindicators.gov/v5/REST.svc/IndicatorDescription/" + str(ind) + "/Indicators/" + str(i) + "?" + auth
-            response = requests.get(url)
-            if response.status_code == 200:
-                    tree = ElementTree.fromstring(response.content)
-                    data = tree.find('Data')
-                    for indicator in data.findall('Indicator'):
-                        fips = str(indicator.find('FIPSCode').text).strip()
-                        print (fips)
-                        if  fips.startswith('51') and len(fips) == 5 and str(indicator.find('DimensionGraphHeader').text).lower() == 'total':
-                            f.write(indicator_map.get(ind)+","+str(indicator.find('FIPSCode').text).strip() + "," + ("" if indicator.find('FloatValue').text is None else str(indicator.find('FloatValue').text).strip()) + "\n")
+
+# Function to invoke the API service, parse the XML response and write the extracted fields to CSV file
+def extract_and_write_to_file():
+    f = open("Indicators.csv", "w")
+    f.write("Indicator Type,County,Value\n")
+    for ind in indicator_map:
+        # Invoking the sepcific API service for each indicator in the indicator map and getting the response
+        response = requests.get("http://services.healthindicators.gov/v5/REST.svc/IndicatorDescription/" + str(ind) + "/Indicators/PageCount?" + auth)
+        if response.status_code == 200:
+            # Parsing the XML response and getting the page count
+            tree = ElementTree.fromstring(response.content)
+            page_count = str(tree.find('Data').text)
+            # Looping over all the pages
+            for i in range(1, int(page_count)):
+                url = "http://services.healthindicators.gov/v5/REST.svc/IndicatorDescription/" + str(ind) + "/Indicators/" + str(i) + "?" + auth
+                # Invoking the service and getting the response
+                response = requests.get(url)
+                if response.status_code == 200:
+                        # Parsing the XML response and extracting the value field pertaining to county level data in VA
+                        tree = ElementTree.fromstring(response.content)
+                        data = tree.find('Data')
+                        for indicator in data.findall('Indicator'):
+                            fips = str(indicator.find('FIPSCode').text).strip()
+                            if  fips.startswith('51') and len(fips) == 5 and str(indicator.find('DimensionGraphHeader').text).lower() == 'total':
+                                # writing to the csv file
+                                f.write(indicator_map.get(ind) + "," + str(indicator.find('FIPSCode').text).strip() + "," + ("" if indicator.find('FloatValue').text is None else str(indicator.find('FloatValue').text).strip()) + "\n")
+    f.close()
+
+# calling the function and populating the csv file    
+extract_and_write_to_file()
+
                             
-f.close()
                          
                         
                 
